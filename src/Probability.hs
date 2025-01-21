18d6e5cdd52f441644907eec6a40f58aec5c2c6e
Stian Øverby <stian@dolphinics.com>
2025-01-21 17:40:11 +0100

    Formatting, minimize import and naming

--- src/Probability.hs
+++ src/Probability.hs
@@ -5,7 +5,7 @@ module Probability
     ) where
 
 import qualified Data.Map as Map
-import Control.Monad.Reader (Reader, ask, runReader, MonadPlus (..))
+import Control.Monad.Reader (Reader, ask, runReader, MonadPlus(mzero, mplus))
 import Data.Ratio (Ratio, (%))
 
 import Syntax
@@ -26,9 +26,9 @@ term `equals` outcome =
         _        -> 0
 
 -- * Implementation
-type Probability = Ratio Int
-type Total       = Int
-type Occurrences = Int
+type Probability      = Ratio Int
+type Total            = Int
+type Occurrences      = Int
 type OccurrenceMap a  = Map.Map a Occurrences
 
 data Outcome = NumericValue Int | TruthValue Bool
@@ -40,17 +40,17 @@ type Name          = String
 type Environment   = Name -> Outcome
 
 data Observed a where
-    Lift   :: (Ord a, Show a) => OccurrenceMap a ->  Observed a
-    Return :: a -> Observed a
-    Bind   :: Observed a -> (a -> Observed b) -> Observed b
-    Zero   :: Observed a
-    Plus   :: Observed a -> Observed a -> Observed a
+    Lift         :: (Ord a, Show a) => OccurrenceMap a ->  Observed a
+    Certainly    :: a -> Observed a
+    Bind         :: Observed a -> (a -> Observed b) -> Observed b
+    Impossible   :: Observed a
+    Plus         :: Observed a -> Observed a -> Observed a
 
 instance Functor Observed where
     fmap = liftM
 
 instance Applicative Observed where
-    pure = Return
+    pure = Certainly
     (<*>) = ap
 
 instance Monad Observed where
@@ -58,11 +58,11 @@ instance Monad Observed where
     (>>=)  = Bind
 
 instance Alternative Observed where
-    empty = Zero
+    empty = Impossible
     (<|>) = Plus
 
 instance MonadPlus Observed where
-  mzero = Zero
+  mzero = Impossible
   mplus = Plus
 
 each :: OccurrenceMap a -> [(a, Occurrences)]
@@ -79,22 +79,22 @@ m `apply` f =
          return (b, occ0 * occ1)
 
 run :: (Ord a) => Observed a -> OccurrenceMap a
-run (Lift     m)            = m
-run (Return   a)            = Map.singleton a 1
-run Zero                    = Map.empty
+run (Lift      m)           = m
+run (Certainly a)           = Map.singleton a 1
+run Impossible              = Map.empty
 run (Plus ma mb)            = run ma `union` run mb
-run (Bind (Lift   m) f)     =     m  `apply`       f
-run (Bind (Return a) f)     = run (f a)
-run (Bind Zero _)           = run Zero
-run (Bind (Plus (Lift   m)   mb        ) f) = run (Bind (Lift (m      `union` run mb)) f)
-run (Bind (Plus ma           (Lift m)  ) f) = run (Bind (Lift (run ma `union`     m )) f)
-run (Bind (Plus (Return a)   mb        ) f) = run (Plus (f a)       (Bind mb f))
-run (Bind (Plus ma           (Return b)) f) = run (Plus (Bind ma f) (f b      ))
-run (Bind (Plus Zero         mb        ) f) = run (Bind mb f)
-run (Bind (Plus ma           Zero      ) f) = run (Bind ma f)
-run (Bind (Plus (Plus ma mb) mc        ) f) = run (Bind (Plus ma (Plus mb mc)) f)
-run (Bind (Plus ma           mb        ) f) = run (Plus (Bind ma f) (Bind mb f) )
-run (Bind (Bind ma f) g)                    = run (Bind ma (\a -> Bind (f a) g))
+run (Bind (Lift      m) f)  =     m  `apply`       f
+run (Bind (Certainly a) f)  = run (f a)
+run (Bind Impossible    _)  = run Impossible
+run (Bind (Plus (Lift   m)    mb           ) f) = run (Bind (Lift (m      `union` run mb)) f)
+run (Bind (Plus ma            (Lift m)     ) f) = run (Bind (Lift (run ma `union`     m )) f)
+run (Bind (Plus (Certainly a) mb           ) f) = run (Plus (f a)       (Bind mb f))
+run (Bind (Plus ma            (Certainly b)) f) = run (Plus (Bind ma f) (f b      ))
+run (Bind (Plus Impossible    mb           ) f) = run (Bind mb f)
+run (Bind (Plus ma            Impossible   ) f) = run (Bind ma f)
+run (Bind (Plus (Plus ma mb)  mc           ) f) = run (Bind (Plus ma (Plus mb mc)) f)
+run (Bind (Plus ma            mb           ) f) = run (Plus (Bind ma f) (Bind mb f) )
+run (Bind (Bind ma f) g)                        = run (Bind ma (\a -> Bind (f a) g))
 
 count :: Annotation -> Total
 count    = fst
@@ -103,14 +103,12 @@ observed :: Annotation -> Observed Outcome
 observed = snd
 
 event :: Annotation -> Outcome -> Maybe Occurrences
-event (_, m) outcome =
-    let m' = run m
-    in Map.lookup outcome m'
+event (_, m) outcome = Map.lookup outcome (run m) 
 
-identical :: Annotation -> Outcome -> Probability
-identical a outcome = case event a outcome of
+chanceOf :: Annotation -> Outcome -> Probability
+chanceOf a outcome = case event a outcome of
   Just k -> k % count a
-  _       -> 0
+  _      -> 0
 
 plus :: Outcome -> Outcome -> Outcome
 (NumericValue m) `plus` (NumericValue n) = NumericValue $ m + n
@@ -130,12 +128,14 @@ domain (Num m n) = NumericValue <$> [m .. n     ]
 domain Bool      = TruthValue   <$> [True, False]
 
 interpret :: Term Type -> Reader Environment Annotation
-interpret (Number  n _) = return (k, m)
-    where k = 1
-          m = return (NumericValue n)
-interpret (Boolean b _) = return (k, m)
-    where k = 1
-          m = return (TruthValue   b)
+interpret (Number  n _) = 
+    let k = 1
+        m = return $ NumericValue n
+    in return (k, m)
+interpret (Boolean b _) =
+    let k = 1
+        m = return $ TruthValue   b
+    in return (k, m)
 interpret (Variable x _) = do
     env <- ask
     let k = 1
@@ -158,14 +158,14 @@ interpret (Conditional t0 t1 t2 _) = do
     a0 <- interpret t0
     a1 <- interpret t1
     a2 <- interpret t2
-    return $ case a0 `identical` TruthValue True of
+    return $ case a0 `chanceOf` TruthValue True of
         1 -> a1
         0 -> a2
         _ -> error "Internal error: A branch has to be picked with probability 1"
 interpret (Let x (Uniform tau) t2 _) = do
     env <- ask
     let annotations   = [runReader (interpret t2) (bind x val env) | val <- domain tau]
-        k             = sum $ map count annotations
+        k             = sum  $ map count annotations
         m             = Lift $ foldr (union . run . observed) mempty annotations
     return (k, m)
 
